9d6f3e14aec416f2fe66dfaacc0e3b2a2d874ca5
zrah <zrah@protonmail.com>
2022-07-15 14:58:27 +0800

    feat: support incremental synchronization

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,60 @@
-use abbs_meta::{db::commit::CommitDb, git::Repository, package, Config};
-use tracing::{debug, Level};
+use abbs_meta::{
+    db::{abbs::AbbsDb, commits::CommitDb},
+    git::Repository,
+    package, Config,
+};
+use std::collections::HashMap;
+use tracing::info;
 
 #[async_std::main]
 async fn main() {
     init_log();
 
     let config = Config::from_file("config.toml").unwrap();
-    let repo = Repository::open(&config.abbs_path, config.thread).unwrap();
+    let repo = Repository::try_from(&config).unwrap();
 
     let commit_db = CommitDb::open(&config.commits_db_path).await.unwrap();
+    let abbs_db = AbbsDb::open(&config).await.unwrap();
+    let pkgs = package::scan_packages(&config.abbs_path);
+
+    // find packages that were deleted in current abbs
+    let old_pkgs = abbs_db.get_packages_name().await.unwrap();
+    let current_pkgs = pkgs.iter().map(|(pkg, _)| pkg.name.clone()).collect();
+    let deleted_packages = old_pkgs.difference(&current_pkgs);
+
+    info!("deleted packages: {:?}", deleted_packages);
+    abbs_db.delete_package_many(deleted_packages).await.unwrap();
+
     let updated_pkgs = commit_db.update(&repo).await.unwrap();
-    debug!("{:?}", updated_pkgs);
+    info!("updated packages: {:?}", updated_pkgs);
 
-    let git_repo = git2::Repository::open(&config.abbs_path).unwrap();
-    package::generate_changelog("rust", &git_repo, &commit_db)
-        .await
-        .unwrap();
+    let mut map: HashMap<_, Vec<_>> = HashMap::new();
+    for pkg in pkgs {
+        if let Some(v) = map.get_mut(&pkg.0.name) {
+            v.push(pkg);
+        } else {
+            map.insert(pkg.0.name.clone(), vec![pkg]);
+        }
+    }
 
-    //let pkgs = package::scan_packages(&config.abbs_path);
-    //println!("{:?}", pkgs);
+    for pkg_name in updated_pkgs {
+        if let Some(v) = map.get(&pkg_name) {
+            for (pkg, spec) in v {
+                let changes = package::scan_package_changes(&pkg.name, &repo, &commit_db)
+                    .await
+                    .unwrap();
+                abbs_db.add_package(pkg, spec, &changes).await.unwrap();
+            }
+        } else {
+            info!("desperated package: {pkg_name}");
+        }
+    }
 }
 
 fn init_log() {
     tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
+        .with_env_filter("sqlx::query=warn,abbs_meta=info")
         .with_file(true)
         .with_line_number(true)
         .init();
-    //let subscriber = FmtSubscriber::builder()
-    //    .with_max_level(Level::DEBUG)
-    //    .with_file(true)
-    //    .with_line_number(true)
-    //    .finish();
-    //tracing::subscriber::set_global_default(subscriber).unwrap();
 }
