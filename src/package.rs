ab616e459f2b5eeba8ce71dd4efcde2df6bbd523
zrah <zrah@protonmail.com>
2022-07-15 14:45:28 +0800

    feat: scan package changelog

--- src/package.rs
+++ src/package.rs
@@ -1,29 +1,34 @@
+use crate::db::commits::CommitDb;
+use crate::git::Repository;
+use abbs_meta_apml::parse;
+use abbs_meta_tree::Package;
+use anyhow::Context;
+use anyhow::Result;
 use std::{
     collections::HashMap,
     fmt::Write,
     fs,
     path::{Path, PathBuf},
 };
-use abbs_meta_apml::parse;
-use abbs_meta_tree::Package;
-use anyhow::Context;
-use anyhow::Result;
-use tracing::{info, log::warn};
-use crate::db::commit::CommitDb;
-use git2::Repository as Git2Repository;
+use tracing::log::warn;
 
-pub fn scan_packages<P: AsRef<Path>>(abbs_path: P) -> Vec<Package> {
+pub fn scan_packages<P: AsRef<Path>>(abbs_path: P) -> Vec<(Package, HashMap<String, String>)> {
     let pkg_dirs = get_tree_dirs(abbs_path).unwrap();
     let mut pkgs = Vec::new();
 
     for (spec_path, defines_path) in pkg_dirs {
+        let mut spec_context = HashMap::new();
+
+        let s = fs::read_to_string(&spec_path).unwrap();
+        parse(&s, &mut spec_context).ok();
+
         let (context, error) = parse_spec_and_defines(&spec_path, &defines_path);
         if let Some(s) = error {
             warn!("{}", s);
         }
 
         match Package::from(&context, &spec_path) {
-            Ok(pkg) => pkgs.push(pkg),
+            Ok(pkg) => pkgs.push((pkg, spec_context)),
             Err(e) => warn!("{}", e),
         }
     }
@@ -31,38 +36,62 @@ pub fn scan_packages<P: AsRef<Path>>(abbs_path: P) -> Vec<Package> {
     pkgs
 }
 
-pub async fn generate_changelog(
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Change {
+    pub pkg_name: String,
+    pub version: String,
+    pub branch: String,
+    pub urgency: String,
+    pub message: String,
+    pub githash: String,
+    pub maintainer_name: String,
+    pub maintainer_email: String,
+    pub timestamp: i64,
+}
+
+pub async fn scan_package_changes(
     pkg_name: &str,
-    repo: &Git2Repository,
+    repo: &Repository,
     commit_db: &CommitDb,
-) -> Result<()> {
-    let result = commit_db.get_package_commits(pkg_name).await.unwrap();
+) -> Result<Vec<Change>> {
+    let changes = commit_db.get_package_commits(pkg_name).await.unwrap();
 
-    for (commit_id, pkg_path) in result {
-        let commit = repo.find_commit(commit_id)?;
-        let spec_path = pkg_path + "/spec";
+    let changes = changes
+        .into_iter()
+        .filter_map(|(commit_id, pkg_version, _, _)| {
+            let commit = repo.find_commit(commit_id).ok()?;
 
-        let entry = match commit.tree()?.get_path(Path::new(&spec_path)) {
-            Ok(entry) => entry,
-            Err(e) => {
-                warn!("cannot get entry {spec_path} at {commit:?} : {}",e.message());
-                continue;
-            }
-        };
+            let githash = commit_id.to_string();
+            let message = commit.message()?.to_string();
+            let maintainer = commit.committer();
+            let maintainer_name = maintainer.name()?.to_string();
+            let maintainer_email = maintainer.email()?.to_string();
+            let timestamp = commit.time().seconds();
+            let version = pkg_version;
+            let urgency = message
+                .find("security")
+                .map_or("medium", |_| "high")
+                .to_string();
+            let pkg_name = pkg_name.to_string();
+            let branch = repo.get_branch().to_string();
 
-        let blob_oid = entry.id();
-        let blob = repo.find_blob(blob_oid)?;
-        let v = blob.content();
-        let spec = std::str::from_utf8(v)?;
+            let change = Change {
+                pkg_name,
+                version,
+                branch,
+                urgency,
+                message,
+                githash,
+                maintainer_name,
+                maintainer_email,
+                timestamp,
+            };
+            Some(change)
+        })
+        .collect();
 
-        let mut context = HashMap::new();
-        parse(spec, &mut context).unwrap();
-
-        let res = context.get("VER");
-        info!("{commit:?} {:?}", res);
-    }
-
-    Ok(())
+    Ok(changes)
 }
 
 fn parse_spec_and_defines(
@@ -144,7 +173,7 @@ fn get_tree_dirs<P: AsRef<Path>>(path: P) -> Result<Vec<(PathBuf, PathBuf)>> {
             let spec_path = pkg_dir.join("spec");
             if !spec_path.is_file() {
                 warn!(
-                    "spec file not found at {} for {}",
+                    "spec file not found in {} for {}. Skipping",
                     spec_path.display(),
                     file.path().display()
                 );
