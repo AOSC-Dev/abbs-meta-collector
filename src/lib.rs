d2b84946914cf077b21166b33f54dc252491ad94
zrah <zrah@protonmail.com>
2022-07-15 14:52:10 +0800

    add: add macro skip_error!() skip_none!() to skip Err() or None in loop

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,30 @@ pub mod db;
 pub mod git;
 pub mod package;
 pub use config::Config;
+
+macro_rules! skip_error {
+    ($res:expr) => {
+        match $res {
+            Ok(val) => val,
+            Err(e) => {
+                tracing::debug!("skip error: {:?}", e);
+                continue;
+            }
+        }
+    };
+}
+
+macro_rules! skip_none {
+    ($res:expr) => {
+        match $res {
+            Some(val) => val,
+            None => {
+                tracing::debug!("skip none");
+                continue;
+            }
+        }
+    };
+}
+
+pub(crate) use skip_error;
+pub(crate) use skip_none;
