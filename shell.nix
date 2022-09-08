4e484c5f0d2f91fcb3571dca36f1278078fa081b
zrah <zrah@protonmail.com>
2022-09-08 16:43:34 +0800

    chore: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,21 @@
-let 
-  nixpkgs = import <nixpkgs> {};
-in
-  with nixpkgs;
-  stdenv.mkDerivation {
-    name = "rust";
-    buildInputs = [ 
-      bintools
-      rustup
-      gcc
-      cargo-watch
-      nixfmt
-      ];
-  }
+let nixpkgs = import <nixpkgs> { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "rust";
+  buildInputs = [
+    bintools
+    rustup
+    gcc
+    cargo-watch
+    nixfmt
+    pkgconfig
+    openssl.dev
+    zlib.dev
+    openssl.out
+  ];
+
+  OPENSSL_DEV = openssl.dev;
+  ZLIB_DEV = zlib.dev;
+  LD_LIBRARY_PATH = "${openssl.out}/lib:${zlib.out}/lib";
+  #LD_PRELOAD = "${jemalloc.out}/lib/libjemalloc.so";
+}
