e6d628e0347695bd5ab1712d215c35bfe0244c87
zrah <zrah@protonmail.com>
2022-09-16 22:08:58 +0800

    chore: remove unused dependencies

--- shell.nix
+++ shell.nix
@@ -9,13 +9,6 @@ stdenv.mkDerivation {
     cargo-watch
     nixfmt
     pkgconfig
-    openssl.dev
-    zlib.dev
-    openssl.out
   ];
-
-  OPENSSL_DEV = openssl.dev;
-  ZLIB_DEV = zlib.dev;
-  LD_LIBRARY_PATH = "${openssl.out}/lib:${zlib.out}/lib";
   #LD_PRELOAD = "${jemalloc.out}/lib/libjemalloc.so";
 }
